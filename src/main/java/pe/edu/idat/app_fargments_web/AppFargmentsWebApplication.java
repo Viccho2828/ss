f18845a4ff9ac654f930c3808e453e41b50099d4
Viccho2828 <kikekollizion@gmail.com>
2024-06-13 17:51:09 -0500

    initial commit1

---
+++ src/main/java/pe/edu/idat/app_fargments_web/AppFargmentsWebApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_fargments_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFargmentsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFargmentsWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/app_fargments_web/AppFargmentsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_fargments_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFargmentsWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
